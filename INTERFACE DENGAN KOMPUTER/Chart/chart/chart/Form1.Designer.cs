826cfe9b62ab7dcc3b220b28a320213c3d794515
qociem <qociem@yahoo.com>
2024-01-13 19:20:09 +0700

    initial

---
+++ INTERFACE DENGAN KOMPUTER/Chart/chart/chart/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(25, 16);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series3.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(642, 414);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ INTERFACE DENGAN KOMPUTER/Chart/chart/chart/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chart
+{
+    public partial class Form1 : Form
+    {
+        int[] x = new int[] {1,2,3,4,5};
+        int[] x2 = new int[] { 1, 2, 3, 4, 5 };
+        int[] x3 = new int[] { 1, 2, 3, 4, 5 };
+        double[] y = new double[] { 1.1 , 2.2, 3.3, 4.2, 5 };
+        double[] y2 = new double[] { 1, 2, 3, 4, 5 };
+        double[] y3 = new double[] { 1, 2.6, 3.9, 4.7, 5 };
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if(count == 1)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[0], y[0]);
+                chart1.Series["Series2"].Points.AddXY(x2[0], y2[0]);
+                chart1.Series["Series3"].Points.AddXY(x3[0], y3[0]);
+            }
+            else if(count ==2)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[1], y[1]);
+                chart1.Series["Series2"].Points.AddXY(x2[1], y2[1]);
+                chart1.Series["Series3"].Points.AddXY(x3[1], y3[1]);
+            }
+            else if (count == 3)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[2], y[2]);
+                chart1.Series["Series2"].Points.AddXY(x2[2], y2[2]);
+                chart1.Series["Series3"].Points.AddXY(x3[2], y3[2]);
+            }
+            else if (count == 4)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[3], y[3]);
+                chart1.Series["Series2"].Points.AddXY(x2[3], y2[3]);
+                chart1.Series["Series3"].Points.AddXY(x3[3], y3[3]);
+            }
+            else if (count == 5)
+            {
+                chart1.Series["Series1"].Points.AddXY(x[4], y[4]);
+                chart1.Series["Series2"].Points.AddXY(x2[4], y2[4]);
+                chart1.Series["Series3"].Points.AddXY(x3[4], y3[4]);
+                timer1.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.AddXY(0, 0);
+            chart1.Series["Series2"].Points.AddXY(0, 0);
+            chart1.Series["Series3"].Points.AddXY(0, 0);
+            chart1.ChartAreas[0].AxisX.Title = "Time (s)";
+            chart1.ChartAreas[0].AxisY.Title = "Voltage (V)";
+
+            chart1.ChartAreas[0].AxisX.Maximum = 6;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 5;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+        }
+    }
+}
